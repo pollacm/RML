9f3feee4417fa0486631edd6feaaca201fae2e44
Clarence Pollard <pollacm@gmail.com>
2018-07-26 23:22:50 -0400

    Getting necessary scores for Power Rankings

--- RML/Program.cs
+++ RML/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -14,6 +16,7 @@ namespace RML
             var driver = new ChromeDriver();
 
             //login
+
             driver.Navigate().GoToUrl($"http://games.espn.com/ffl/standings?leagueId=127291&seasonId={year}");
             driver.SwitchTo().Frame("disneyid-iframe");
             var userNameBox = driver.FindElement(By.CssSelector("div.field-username-email input"));
@@ -23,60 +26,61 @@ namespace RML
             var passwordBox = driver.FindElement(By.CssSelector("div.field-password input"));
             passwordBox.SendKeys(Keys.ArrowDown);
             passwordBox.SendKeys("grip1334");
-
-            var submitButton = driver.FindElement(By.XPath("//button[contains(., 'Log In')]"));
-            submitButton.Click();
+            passwordBox.SendKeys(Keys.Enter);
+            System.Threading.Thread.Sleep(2000);
+            //var submitButton = driver.FindElement(By.XPath("//button[contains(., 'Log In')]"));
+            //submitButton.Click();
 
 
 
             //get teams
-            var teamAnchors = driver.FindElements(By.CssSelector("div.games-fullcol table:nth-child(1) a"));
+            //var teamAnchors = driver.FindElements(By.CssSelector("div.games-fullcol table:nth-child(1) a"));
 
-            var teams = new List<string>();
-            foreach (var teamAnchor in teamAnchors)
-            {
-                teams.Add(teamAnchor.Text);
-            }
+            //var teams = new List<string>();
+            //foreach (var teamAnchor in teamAnchors)
+            //{
+            //    teams.Add(teamAnchor.Text);
+            //}
 
-            driver.WaitUntilElementExists(By.CssSelector("table.tableBody"));
+            //driver.WaitUntilElementExists(By.CssSelector("table.tableBody"));
 
             //OP
-            driver.Navigate().GoToUrl($"http://games.espn.com/ffl/freeagency?leagueId=127291&seasonId={year}");
-            driver.WaitUntilElementExists(By.Id("playerTableContainerDiv"));
+            //driver.Navigate().GoToUrl($"http://games.espn.com/ffl/freeagency?leagueId=127291&seasonId={year}");
+            //driver.WaitUntilElementExists(By.Id("playerTableContainerDiv"));
 
-            var opLink = driver.FindElement(By.XPath("//ul[@class='filterToolsOptionSet']/li/a[contains(.,'OP')]"));
-            opLink.Click();
+            //var opLink = driver.FindElement(By.XPath("//ul[@class='filterToolsOptionSet']/li/a[contains(.,'OP')]"));
+            //opLink.Click();
 
-            System.Threading.Thread.Sleep(2000);
+            //System.Threading.Thread.Sleep(2000);
 
-            var lastLink = driver.FindElement(By.XPath("//tr[contains(@class, 'playerTableBgRowSubhead')]/td/a[contains(.,'LAST')]"));
-            lastLink.Click();
+            //var lastLink = driver.FindElement(By.XPath("//tr[contains(@class, 'playerTableBgRowSubhead')]/td/a[contains(.,'LAST')]"));
+            //lastLink.Click();
 
-            System.Threading.Thread.Sleep(5000);
+            //System.Threading.Thread.Sleep(5000);
 
             //TODO: Need to account for more than one
-            var opRow = driver.FindElements(By.XPath("//tr[contains(@class, 'pncPlayerRow')]/td[3][not(contains(.,'FA'))]/parent::tr")).First();
-            var opPlayerName = opRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).Text;
-            var opPlayerTeam = opRow.FindElement(By.XPath("./td[3]/a")).GetAttribute("title");
-            var opPlayerTeamAbbreviation = opRow.FindElement(By.XPath("./td[3]/a")).Text;
-            var opPlayerId = opRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).GetAttribute("playerid");
-            var opPlayerPoints = opRow.FindElement(By.XPath("./td[contains(@class, 'sortedCell')]")).Text;
+            //var opRow = driver.FindElements(By.XPath("//tr[contains(@class, 'pncPlayerRow')]/td[3][not(contains(.,'FA'))]/parent::tr")).First();
+            //var opPlayerName = opRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).Text;
+            //var opPlayerTeam = opRow.FindElement(By.XPath("./td[3]/a")).GetAttribute("title");
+            //var opPlayerTeamAbbreviation = opRow.FindElement(By.XPath("./td[3]/a")).Text;
+            //var opPlayerId = opRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).GetAttribute("playerid");
+            //var opPlayerPoints = opRow.FindElement(By.XPath("./td[contains(@class, 'sortedCell')]")).Text;
 
             //DP
-            var dpLink = driver.FindElement(By.XPath("//ul[@class='filterToolsOptionSet']/li/a[contains(.,'DP')]"));
-            dpLink.Click();
+            //var dpLink = driver.FindElement(By.XPath("//ul[@class='filterToolsOptionSet']/li/a[contains(.,'DP')]"));
+            //dpLink.Click();
 
-            System.Threading.Thread.Sleep(2000);
-            
-            var dpRow = driver.FindElements(By.XPath("//tr[contains(@class, 'pncPlayerRow')]/td[3][not(contains(.,'FA'))]/parent::tr")).First();
-            var dpPlayerName = dpRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).Text;
-            var dpPlayerTeam = dpRow.FindElement(By.XPath("./td[3]/a")).GetAttribute("title");
-            var dpPlayerTeamAbbreviation = dpRow.FindElement(By.XPath("./td[3]/a")).Text;
-            var dpPlayerId = dpRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).GetAttribute("playerid");
-            var dpPlayerPoints = dpRow.FindElement(By.XPath("./td[contains(@class, 'sortedCell')]")).Text;
+            //System.Threading.Thread.Sleep(2000);
+
+            //var dpRow = driver.FindElements(By.XPath("//tr[contains(@class, 'pncPlayerRow')]/td[3][not(contains(.,'FA'))]/parent::tr")).First();
+            //var dpPlayerName = dpRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).Text;
+            //var dpPlayerTeam = dpRow.FindElement(By.XPath("./td[3]/a")).GetAttribute("title");
+            //var dpPlayerTeamAbbreviation = dpRow.FindElement(By.XPath("./td[3]/a")).Text;
+            //var dpPlayerId = dpRow.FindElement(By.XPath("./td[@class='playertablePlayerName']/a")).GetAttribute("playerid");
+            //var dpPlayerPoints = dpRow.FindElement(By.XPath("./td[contains(@class, 'sortedCell')]")).Text;
 
             //TODO: Power Rankings
-            //var powerRankings = GetPowerRankings();
+            GetPowerRankings(driver);
             //TODO: Assign Trophies
             AssignTrophies();
             var x = 1;
@@ -90,39 +94,68 @@ namespace RML
         private static void GetPowerRankings(ChromeDriver driver)
         {
             var currentWeek = week;
-            for (int i = 0; i < 3; i++)
+            List<Score>[] scoresForPowerRankings = new List<Score>[4];
+            //TODO: Get previous power ranking
+            for (int i = 3; i >= 0; i--)
             {
-                List<Score> scores = GetScores(currentWeek, driver);
-                
-
-
-                currentWeek--;
-                if (currentWeek == 0)
-                {
-                    break;
-                }
+                scoresForPowerRankings[i] = GetScores(currentWeek - i, driver);
             }
+
+            var x = 1;
         }
 
         private static List<Score> GetScores(int currentWeek, ChromeDriver driver)
         {
             var scores = new List<Score>();
+            
+            driver.Navigate().GoToUrl($"http://games.espn.com/ffl/leagueoffice?leagueId=127291&seasonId={year}");
+            driver.WaitUntilElementExists(By.ClassName("games-nav"));
 
             driver.Navigate().GoToUrl($"http://games.espn.com/ffl/scoreboard?leagueId=127291&matchupPeriodId={currentWeek}");
-            driver.WaitUntilElementExists(By.ClassName("ptsBased matchup"));
+            driver.WaitUntilElementExists(By.ClassName("ptsBased"));
 
-            var matchups = driver.FindElements(By.XPath("td[@class='team']"));
-
+            var matchups = driver.FindElements(By.XPath("//table[@class='ptsBased matchup']"));
             foreach (var matchup in matchups)
             {
-                
+                scores.Add(BuildMatchup(matchup));
             }
-            //td[@class='team']
-            //name => ./div[@class='name']/a   split " ("[0]
-            //abbrev => ./span Text
-            //score => //table[@class='ptsBased matchup']/tbody/tr[x]/td[2]
-            //win contains class winning
+            
             return scores;
+        }
+
+        private static Score BuildMatchup(IWebElement matchupElement)
+        {
+            var score = new Score();
+            var teams = matchupElement.FindElements(By.XPath("./tbody//tr"));
+            var awayTeam = true;
+            foreach (var team in teams)
+            {
+                if (awayTeam) //new score; build away
+                {
+                    score = new Score();
+                    score.AwayTeam = BuildScore(team);
+                    awayTeam = false;
+                }
+                else //build home team
+                {
+                    score.HomeTeam = BuildScore(team);
+                    break;
+                }
+            }
+
+            return score;
+        }
+
+        private static Team BuildScore(IWebElement teamElement)
+        {
+            var team = new Team();
+
+            team.TeamName = teamElement.FindElement(By.XPath("./td[@class='team']/div[@class='name']/a")).Text;
+            team.TeamAbbreviation = teamElement.FindElement(By.XPath("./td[@class='team']/div[@class='name']/span")).Text.Replace("(","").Replace(")","");
+            team.TeamPoints = decimal.Parse(teamElement.FindElement(By.XPath("./td[contains(@class, 'score')]")).Text);
+            team.Win = teamElement.FindElements(By.XPath("./td[2][contains(@class, 'winning')]")).Any();
+
+            return team;
         }
     }
 
--- RML/Team.cs
+++ RML/Team.cs
@@ -12,5 +12,13 @@ namespace RML
         public string TeamAbbreviation { get; set; }
         public decimal TeamPoints { get; set; }
         public bool Win { get; set; }
+
+        public decimal GetPointsForWeek 
+        {
+            get
+            {
+                return TeamPoints + (Win ? 50 : 0);
+            }
+        }
     }
 }
