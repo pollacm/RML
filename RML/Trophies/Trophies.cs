f7b49d19412506def02f29080855f014b315c0f0
Clarence Pollard <pollacm@gmail.com>
2018-07-28 21:50:52 -0400

    Adding global names of trophies

--- RML/Trophies/Trophies.cs
+++ RML/Trophies/Trophies.cs
@@ -8,69 +8,90 @@ namespace RML.Trophies
 {
     public class Trophies
     {
-        List<Trophy> TrophyList = new List<Trophy>
-        {
-            new Trophy
-            {
-                TrophyName = "Da 500 Club"
-            },
-            new Trophy
-            {
-                TrophyName = "DA' 600 CLUB!!!!!!!!!"
-            },
-            new Trophy
-            {
-                TrophyName = "DA' 700 CLUB"
-            },
-            new Trophy
-            {
-                TrophyName = "Rake It Up!"
-            },
-            new Trophy
-            {
-                TrophyName = "Meek Millz!!!"
-            },
-            new Trophy
-            {
-                TrophyName = "Offensive Player of the Week Award!!!!"
-            },
+        public string Da500Club = "Da 500 Club";
+        public string Da600Club = "Da 600 Club";
+        public string Da700Club = "Da 700 Club";
+        public string LargestMarginWeekWinner = "Rake It Up!";
+        public string LargestMarginWeekLoser = "Meek Millz!!!";
+        public string OffensivePlayerOfTheWeek = "Offensive Player of the Week Award!!!!";
+        public string DefensivePlayerOfTheWeek = "Defensive Player of the Week Award!!!!";
+        public string Number12Ranked = "Meek Millz of the Year Award!!!";
+        public string Number1Seed = "MASK OFF!!!";
+        public string OffensivePlayerOfTheYear = "R.M.L. Offensive MVP";
+        public string DefensivePlayerOfTheYear = "R.M.L. Defensive MVP";
+        public string HighestScoringTeamOfTheYear = "Secure The Bag!!!!";
+        public string AfcChamp = "AFC Division Championship Banner";
+        public string NfcChamp = "NFC Division Championship Banner";
+        public string Champion = "R.M.L. Championship Trophy";
 
-            new Trophy
+        public List<Trophy> TrophyList;
+
+        public Trophies()
+        {
+            TrophyList = new List<Trophy>
             {
-                TrophyName = "Defensive Player of the Week Award!!!!"
-            },
-            new Trophy
-            {
-                TrophyName = "Meek Millz of the Year Award!!!"
-            },
-            new Trophy
-            {
-                TrophyName = "MASK OFF!!!"
-            },
-            new Trophy
-            {
-                TrophyName = "R.M.L. Offensive MVP"
-            },
-            new Trophy
-            {
-                TrophyName = "R.M.L. Defensive MVP"
-            },
-            new Trophy
-            {
-                TrophyName = "Secure The Bag!!!!"
-            },
-            new Trophy
-            {
-                TrophyName = "AFC Division Championship Banner"
-            },
-            new Trophy
-            {
-                TrophyName = "NFC Division Championship Banner"
-            },
-            new Trophy
-            {
-                TrophyName = "R.M.L. Championship Trophy"
-            },
-        };
+                new Trophy
+                {
+                    TrophyName = Da500Club
+                },
+                new Trophy
+                {
+                    TrophyName = Da600Club
+                },
+                new Trophy
+                {
+                    TrophyName = Da700Club
+                },
+                new Trophy
+                {
+                    TrophyName = LargestMarginWeekWinner
+                },
+                new Trophy
+                {
+                    TrophyName = LargestMarginWeekLoser
+                },
+                new Trophy
+                {
+                    TrophyName = OffensivePlayerOfTheWeek
+                },
+
+                new Trophy
+                {
+                    TrophyName = DefensivePlayerOfTheWeek
+                },
+                new Trophy
+                {
+                    TrophyName = Number12Ranked
+                },
+                new Trophy
+                {
+                    TrophyName = Number1Seed
+                },
+                new Trophy
+                {
+                    TrophyName = OffensivePlayerOfTheYear
+                },
+                new Trophy
+                {
+                    TrophyName = DefensivePlayerOfTheYear
+                },
+                new Trophy
+                {
+                    TrophyName = HighestScoringTeamOfTheYear
+                },
+                new Trophy
+                {
+                    TrophyName = AfcChamp
+                },
+                new Trophy
+                {
+                    TrophyName = NfcChamp
+                },
+                new Trophy
+                {
+                    TrophyName = Champion
+                },
+            }; ;
+        }
     }
 }
